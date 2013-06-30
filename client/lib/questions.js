45bab7f518f93e912c9e5926da0016df4045acf2
Elomar Souza <elomar@elomar.me>
2013-06-29 20:19:10 -0700

    initial implementation of a questions page

---
+++ client/lib/questions.js
@@ -0,0 +1,53 @@
+Template.questions.loggedIn = function() {
+  return Meteor.userId();
+};
+
+Template.questions.unansweredQuestions = function() {
+  return Questions.find({ $where: function() {
+    return this.answeredBy.indexOf(Meteor.userId()) < 0;
+  }});
+};
+
+Template.questions.answers = function() {
+  return Answers.find({ user_id: Meteor.userId() });
+};
+
+Template.question.answer = function() {
+  var answer = Answers.findOne({
+    question_id: this._id,
+    user_id: Meteor.user()._id });
+  if (answer !== undefined) {
+    return answer
+  } else {Id
+    return { answer: undefined }
+  }
+};
+
+Template.question.events({
+  'click .answer': function(e) {
+    var answer = $(e.toElement).data("answer");
+    Meteor.call("answer", { question_id: this._id, answer: answer }, function(error, id) {
+    });
+  }
+});
+
+Template.answer.text = function() {
+  return Questions.findOne({ _id: this.question_id }).text;
+};
+
+Template.answer.secondaryOrSuccess = function() {
+  if (this.answer) {
+    return "success";
+  } else {
+    return "secondary";
+  }
+};
+
+Template.answer.secondaryOrAlert = function() {
+  if (this.answer) {
+    return "secondary";
+  } else {
+    return "alert";
+  }
+};
+
--- model.js
+++ model.js
@@ -7,3 +7,22 @@ rooms = new Meteor.Collection("rooms");
 events = new Meteor.Collection("events");
 
 teams = new Meteor.Collection("teams");
+
+Questions = new Meteor.Collection("questions");
+Answers = new Meteor.Collection("answers");
+
+Meteor.methods({
+  answer: function(params) {
+    var question = Questions.findOne({ _id: params.question_id });
+    question.answeredBy.push(Meteor.user()._id);
+
+    Questions.update(params.question_id, {
+      $set: {
+        answeredBy: question.answeredBy } } );
+
+    Answers.insert({
+      question_id: params.question_id,
+      user_id: Meteor.user()._id,
+      answer: params.answer});
+  }
+});
