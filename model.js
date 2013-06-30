bac953d56db9962321ca6b490cbf436a36fe012c
Ronaldo Barbachano <ronaldo@redcapmedia.com>
2013-06-29 17:12:29 -0700

    create first set of databases

---
+++ model.js
@@ -0,0 +1,9 @@
+profiles = new Meteor.Collection("profiles");
+
+chat = new Meteor.Collection("chat");
+
+rooms = new Meteor.Collection("rooms");
+
+events = new Meteor.Collection("events");
+
+teams = new Meteor.Collection("teams");
